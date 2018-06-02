da693866fc29af9c7a4b00f8234338e22923a9d2
Adam Khazi <adamk4@gmail.com>
2018-06-02 16:44:50 +0100

    load only tracks into df

--- data.py
+++ data.py
@@ -13,3 +13,17 @@ class Data(object):
                 data = json.load(f)
                 playlistData.append(data)
         return playlistData
+
+    def loadTracks(self, nr_tracks=1000):
+        dataFileNames = os.listdir(self.DATASET_DIR)
+        trackData = []
+        for currFile in dataFileNames:
+            with open(self.DATASET_DIR + currFile) as f:
+                data = json.load(f)
+                for playlist in data['playlists']:
+                    for track in playlist['tracks']:
+                        trackData.append(track)
+
+                        if len(trackData) >= nr_tracks:
+                            return trackData
+        return trackData
