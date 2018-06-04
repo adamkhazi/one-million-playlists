69b0394b5e672f63f6b2974b39439ce7f68c93d2
Adam Khazi <adamk4@gmail.com>
2018-06-04 14:05:13 +0100

    removed path names

--- data.py
+++ data.py
@@ -2,23 +2,27 @@ import os
 import json
 
 class Data(object):
-    DATASET_DIR = "C:/Users/Adam Khazi/Dropbox/University College London/MSc/Dissertation/Datasets/data/"
+    def getDatasetPath(self):
+        paths = open("../dataset_paths.config").read().split('\n')
+        return paths[0]
 
     # 1000 playlists per file
     def load(self, nr_files=1):
-        dataFileNames = os.listdir(self.DATASET_DIR)
+        dataDir = self.getDatasetPath()
+        dataFileNames = os.listdir(dataDir)
         playlistData = []
         for i in range(nr_files):
-            with open(self.DATASET_DIR + dataFileNames[i]) as f:
+            with open(dataDir + dataFileNames[i]) as f:
                 data = json.load(f)
                 playlistData.append(data)
         return playlistData
 
     def loadTracks(self, nr_files=1):
-        dataFileNames = os.listdir(self.DATASET_DIR)
+        dataDir = self.getDatasetPath()
+        dataFileNames = os.listdir(dataDir)
         trackData = []
         for i in range(nr_files):
-            with open(self.DATASET_DIR + dataFileNames[i]) as f:
+            with open(dataDir + dataFileNames[i]) as f:
                 data = json.load(f)
                 for playlist in data['playlists']:
                     for track in playlist['tracks']:
