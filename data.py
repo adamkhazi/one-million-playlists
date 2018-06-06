1da2aa32935083c1f469ca7c2bb9f75b87a5ec7f
Adam Khazi <adamk4@gmail.com>
2018-06-06 16:42:22 +0100

    better memmgmt loading data

--- data.py
+++ data.py
@@ -1,7 +1,14 @@
 import os
 import json
+from datetime import datetime
+import pandas as pd
+from tqdm import tqdm
 
 class Data(object):
+    def getPlaylistDfPath(self):
+        paths = open("../dataset_paths.config").read().split('\n')
+        return paths[1]
+
     def getDatasetPath(self):
         paths = open("../dataset_paths.config").read().split('\n')
         return paths[0]
@@ -28,3 +35,48 @@ class Data(object):
                     for track in playlist['tracks']:
                         trackData.append(track)
         return trackData
+
+    def loadFormattedPlaylists(self, nr_files=1):
+        dataDir = self.getDatasetPath()
+        dataFileNames = os.listdir(dataDir)
+
+        playlistDataDF = pd.DataFrame(columns=['num_artists', 'num_albums', 'num_tracks',
+        'num_followers', 'num_edits', 'modified_at', 'modified_at_month', 'modified_at_year',
+        'modified_at_day', 'duration_ms', 'duration_sec', 'duration_min', 'collaborative'])
+
+        for i in tqdm(range(nr_files)):
+            with open(dataDir + dataFileNames[i]) as f:
+                playlistSet = json.load(f)
+                playListSetRows = []
+
+                origLen = len(playlistSet['playlists'])
+                for _ in range(origLen):
+                    row = dict()
+                    row['num_artists'] = playlistSet["playlists"][0]['num_artists']
+                    row['num_albums'] = playlistSet["playlists"][0]['num_albums']
+                    row['num_tracks'] = playlistSet["playlists"][0]['num_tracks']
+                    row['num_followers'] = playlistSet["playlists"][0]['num_followers']
+                    row['num_edits'] = playlistSet["playlists"][0]['num_edits']
+                    row['modified_at'] = playlistSet["playlists"][0]['modified_at']
+                    row['modified_at_month'] = int(datetime.fromtimestamp(playlistSet["playlists"][0]['modified_at']).strftime('%m'))
+                    row['modified_at_year'] = int(datetime.fromtimestamp(playlistSet["playlists"][0]['modified_at']).strftime('%Y'))
+                    row['modified_at_day'] = int(datetime.fromtimestamp(playlistSet["playlists"][0]['modified_at']).strftime('%d'))
+                    row['duration_ms'] = playlistSet["playlists"][0]['duration_ms']
+                    row['duration_sec'] = playlistSet["playlists"][0]['duration_ms'] / 1000
+                    row['duration_min'] = (playlistSet["playlists"][0]['duration_ms'] / 1000)/60
+                    row['collaborative'] = playlistSet["playlists"][0]['collaborative']
+                    
+                    del playlistSet["playlists"][0]
+                    playListSetRows.append(row)
+
+                playlistDataDF = playlistDataDF.append(playListSetRows, ignore_index=True)
+                print('rows: ', playlistDataDF.shape[0])
+        return playlistDataDF
+
+    def savePlaylistDf(self, df):
+        path = self.getPlaylistDfPath()
+        df.to_pickle(path)
+    
+    def loadPlaylistDf(self):
+        path = self.getPlaylistDfPath()
+        return pd.read_pickle(path)
