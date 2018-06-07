4533dff9f7e2d4cebb4980fb4a910f75ecf62359
Adam Khazi <adamk4@gmail.com>
2018-06-07 11:47:33 +0100

    api auth

--- api.py
+++ api.py
@@ -1,11 +1,15 @@
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 class API:
     def __init__(self):
         self.__config = dict(line.strip().split('=') for line in open("../project.config"))
-        self.__sp = spotipy.Spotify(auth=self.__config['SPOTIFY_API_KEY'])
-        self.__sp.trace = True
-        self.__sp.trace_out = True
+        client_credentials_manager = SpotifyClientCredentials(
+            client_id=self.__config['SPOTIFY_API_CLIENT_ID'],
+                client_secret=self.__config['SPOTIFY_API_CLIENT_SECRET'])
+        self.__sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        self.__sp.trace = False
+        self.__sp.trace_out = False
 
     def getTrackInfo(self, trackURI):
         track = self.__sp.track(trackURI)
