23f77ae6d204b8ecb76e5b72a2172b726be0487e
Adam Khazi <adamk4@gmail.com>
2018-08-09 12:20:50 +0100

    dev start windows personal paths incl

---
+++ dev_start.bat
@@ -0,0 +1,3 @@
+start "" "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe"
+start "" "C:\Program Files\Microsoft VS Code\Code.exe"
+"%windir%\System32\cmd.exe" /K ""C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\Users\Adam Khazi\AppData\Local\conda\conda\envs\py36"" "cd C:\Users\Adam Khazi\Desktop\ucl-dissertation-code & jupyter notebook"
